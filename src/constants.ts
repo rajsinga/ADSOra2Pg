62b4d0e819843484a0b56ef5c8564947fc5aab12
rajesh <rajeshsi@microsoft.com>
2021-10-11 00:45:17 +0530

    adding constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { localize } from './localization';
+
+export const extensionName = localize('constants.extensionName', 'Extension for Ora2Pg');
